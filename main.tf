bb3e65aead57848f36b8e4b7840afcbd4d03c50d
Jason Spriggs <jason.spriggs@magfest.org>
2022-11-04 20:29:13 -0400

    Fix load_balancer_arn ref

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_lb_target_group" "ubersystem_web" {
 }
 
 resource "aws_lb_listener" "ubersystem_web" {
-  load_balancer_arn = aws_lb.front_end.arn
+  load_balancer_arn = aws_lb.ubersystem.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
