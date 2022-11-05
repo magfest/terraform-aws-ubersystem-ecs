e16e8e678bbf3d657451492db7995f91c8de4574
Jason Spriggs <jason.spriggs@magfest.org>
2022-11-04 20:17:36 -0400

    Fix target group type

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "aws_lb_target_group" "ubersystem_web" {
   name_prefix   = "uber"
   port          = 80
   protocol      = "HTTP"
+  target_type   = "ip"
   vpc_id        = var.vpc_id
 }
 
