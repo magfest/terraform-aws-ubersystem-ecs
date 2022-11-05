fd7bf9d92d3d77cfe826d7c1eea9f05632b84ea3
Jason Spriggs <jason.spriggs@magfest.org>
2022-11-05 10:22:47 -0400

    Switch to NLB instead of app mesh, maybe one day

--- main.tf
+++ main.tf
@@ -31,16 +31,6 @@ resource "aws_lb" "ubersystem" {
   subnets            = var.subnet_ids
 
   enable_deletion_protection = false
-
-  # access_logs {
-  #   bucket  = aws_s3_bucket.lb_logs.bucket
-  #   prefix  = "test-lb"
-  #   enabled = true
-  # }
-
-  # tags = {
-  #   Environment = "production"
-  # }
 }
 
 resource "aws_lb_target_group" "ubersystem_web" {
@@ -62,6 +52,15 @@ resource "aws_lb_listener" "ubersystem_web" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ubersystem_web.arn
   }
+}
+
+resource "aws_lb" "ubersystem_internal" {
+  name_prefix        = "uber"
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
 }
 
 # -------------------------------------------------------------------
@@ -120,11 +119,11 @@ resource "aws_ecs_task_definition" "ubersystem_web" {
       },
       {
         "name": "SESSION_HOST",
-        "value": "${aws_appmesh_virtual_service.redis.name}"
+        "value": "${aws_lb.ubersystem_internal.dns_name}"
       },
       {
         "name": "BROKER_HOST",
-        "value": "${aws_appmesh_virtual_service.rabbitmq.name}"
+        "value": "${aws_lb.ubersystem_internal.dns_name}"
       }
     ],
     "secrets": [
@@ -135,44 +134,7 @@ resource "aws_ecs_task_definition" "ubersystem_web" {
     ],
     "image": "${var.ubersystem_container}",
     "essential": true,
-    "name": "web",
-    "dependsOn": [ 
-      { 
-        "containerName": "envoy",
-        "condition": "HEALTHY" 
-      }
-    ]
-  },
-  {
-    "logConfiguration": {
-      "logDriver": "awslogs",
-      "secretOptions": null,
-      "options": {
-        "awslogs-group": "/ecs/Ubersystem",
-        "awslogs-region": "us-east-1",
-        "awslogs-stream-prefix": "ecs"
-      }
-    },
-    "environment": [
-      {
-        "name": "APPMESH_VIRTUAL_NODE_NAME",
-        "value": "${aws_appmesh_virtual_node.web.arn}"
-      }
-    ],
-    "image": "public.ecr.aws/appmesh/aws-appmesh-envoy:v1.23.1.0-prod",
-    "healthCheck": {
-      "retries": 3,
-      "command": [
-        "CMD-SHELL",
-        "curl -s http://localhost:9901/server_info | grep state | grep -q LIVE"
-      ],
-      "timeout": 2,
-      "interval": 5,
-      "startPeriod": 10
-    },
-    "essential": true,
-    "user": "1337",
-    "name": "envoy"
+    "name": "web"
   }
 ]
 TASK_DEFINITION
@@ -188,17 +150,6 @@ TASK_DEFINITION
   ]
 
   task_role_arn = aws_iam_role.task_role.arn
-  proxy_configuration {
-    type           = "APPMESH"
-    container_name = "envoy"
-    properties = {
-      AppPorts         = "8282"
-      EgressIgnoredIPs = "169.254.170.2,169.254.169.254"
-      IgnoredUID       = "1337"
-      ProxyEgressPort  = 15001
-      ProxyIngressPort = 15000
-    }
-  }
 
   # volume {
   #   name = "static-files"
@@ -220,82 +171,6 @@ TASK_DEFINITION
     cpu_architecture        = "X86_64"
   }
 }
-
-resource "aws_appmesh_virtual_service" "web" {
-  name      = "web.${aws_appmesh_mesh.ubersystem.name}"
-  mesh_name = aws_appmesh_mesh.ubersystem.id
-
-  spec {
-    provider {
-      virtual_node {
-        virtual_node_name = aws_appmesh_virtual_node.web.name
-      }
-    }
-  }
-}
-
-resource "aws_appmesh_virtual_node" "web" {
-  name      = "web"
-  mesh_name = aws_appmesh_mesh.ubersystem.id
-
-  spec {
-    backend {
-      virtual_service {
-        virtual_service_name = aws_appmesh_virtual_service.rabbitmq.name
-      }
-    }
-
-    backend {
-      virtual_service {
-        virtual_service_name = aws_appmesh_virtual_service.redis.name
-      }
-    }
-
-    listener {
-      port_mapping {
-        port     = 8282
-        protocol = "tcp"
-      }
-    }
-
-    service_discovery {
-      dns {
-        hostname = "web" #Container name
-      }
-    }
-  }
-}
-
-# resource "aws_appmesh_virtual_router" "web" {
-#   name      = "web"
-#   mesh_name = aws_appmesh_mesh.ubersystem.id
-
-#   spec {
-#     listener {
-#       port_mapping {
-#         port     = 8282
-#         protocol = "tcp"
-#       }
-#     }
-#   }
-# }
-
-# resource "aws_appmesh_route" "web" {
-#   name                = "web"
-#   mesh_name           = aws_appmesh_mesh.ubersystem.id
-#   virtual_router_name = aws_appmesh_virtual_router.web.name
-
-#   spec {
-#     tcp_route {
-#       action {
-#         weighted_target {
-#           virtual_node = aws_appmesh_virtual_node.web.name
-#           weight       = 100
-#         }
-#       }
-#     }
-#   }
-# }
 
 
 # -------------------------------------------------------------------
@@ -420,6 +295,12 @@ resource "aws_ecs_service" "rabbitmq" {
     security_groups   = var.security_groups
     assign_public_ip  = true
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.rabbitmq.arn
+    container_name   = "rabbitmq"
+    container_port   = 5672
+  }
 }
 
 resource "aws_ecs_task_definition" "rabbitmq" {
@@ -459,44 +340,7 @@ resource "aws_ecs_task_definition" "rabbitmq" {
     ],
     "image": "public.ecr.aws/docker/library/rabbitmq:alpine",
     "essential": true,
-    "name": "rabbitmq",
-    "dependsOn": [ 
-      { 
-        "containerName": "envoy",
-        "condition": "HEALTHY" 
-      }
-    ] 
-  },
-  {
-    "logConfiguration": {
-      "logDriver": "awslogs",
-      "secretOptions": null,
-      "options": {
-        "awslogs-group": "/ecs/Ubersystem",
-        "awslogs-region": "us-east-1",
-        "awslogs-stream-prefix": "ecs"
-      }
-    },
-    "environment": [
-      {
-        "name": "APPMESH_VIRTUAL_NODE_NAME",
-        "value": "${aws_appmesh_virtual_node.rabbitmq.arn}"
-      }
-    ],
-    "image": "public.ecr.aws/appmesh/aws-appmesh-envoy:v1.23.1.0-prod",
-    "healthCheck": {
-      "retries": 3,
-      "command": [
-        "CMD-SHELL",
-        "curl -s http://localhost:9901/server_info | grep state | grep -q LIVE"
-      ],
-      "timeout": 2,
-      "interval": 5,
-      "startPeriod": 10
-    },
-    "essential": true,
-    "user": "1337",
-    "name": "envoy"
+    "name": "rabbitmq"
   }
 ]
 TASK_DEFINITION
@@ -513,80 +357,24 @@ TASK_DEFINITION
   }
 
   task_role_arn = aws_iam_role.task_role.arn
-  proxy_configuration {
-    type           = "APPMESH"
-    container_name = "envoy"
-    properties = {
-      AppPorts         = "5672"
-      EgressIgnoredIPs = "169.254.170.2,169.254.169.254"
-      IgnoredUID       = "1337"
-      ProxyEgressPort  = 15001
-      ProxyIngressPort = 15000
-    }
-  }
 }
 
-resource "aws_appmesh_virtual_service" "rabbitmq" {
-  name      = "rabbitmq.${aws_appmesh_mesh.ubersystem.name}"
-  mesh_name = aws_appmesh_mesh.ubersystem.id
-
-  spec {
-    provider {
-      virtual_node {
-        virtual_node_name = aws_appmesh_virtual_node.rabbitmq.name
-      }
-    }
-  }
+resource "aws_lb_target_group" "rabbitmq" {
+  name_prefix = "rabbit"
+  port        = 5672
+  protocol    = "TCP"
+  target_type = "ip"
+  vpc_id      = var.vpc_id
 }
 
-resource "aws_appmesh_virtual_node" "rabbitmq" {
-  name      = "rabbitmq"
-  mesh_name = aws_appmesh_mesh.ubersystem.id
+resource "aws_lb_listener" "rabbitmq" {
+  load_balancer_arn = aws_lb.ubersystem_internal.arn
+  port              = "5672"
+  protocol          = "TLS"
 
-  spec {
-    listener {
-      port_mapping {
-        port     = 5672
-        protocol = "tcp"
-      }
-    }
-
-    service_discovery {
-      dns {
-        hostname = "rabbitmq" #Container name
-      }
-    }
-  }
-}
-
-resource "aws_appmesh_virtual_router" "rabbitmq" {
-  name      = "rabbitmq"
-  mesh_name = aws_appmesh_mesh.ubersystem.id
-
-  spec {
-    listener {
-      port_mapping {
-        port     = 5672
-        protocol = "tcp"
-      }
-    }
-  }
-}
-
-resource "aws_appmesh_route" "rabbitmq" {
-  name                = "rabbitmq"
-  mesh_name           = aws_appmesh_mesh.ubersystem.id
-  virtual_router_name = aws_appmesh_virtual_router.rabbitmq.name
-
-  spec {
-    tcp_route {
-      action {
-        weighted_target {
-          virtual_node = aws_appmesh_virtual_node.rabbitmq.name
-          weight       = 100
-        }
-      }
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.rabbitmq.arn
   }
 }
 
@@ -606,6 +394,12 @@ resource "aws_ecs_service" "redis" {
     subnets           = var.subnet_ids
     security_groups   = var.security_groups
     assign_public_ip  = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.redis.arn
+    container_name   = "redis"
+    container_port   = 6379
   }
 }
 
@@ -638,44 +432,7 @@ resource "aws_ecs_task_definition" "redis" {
     ],
     "image": "public.ecr.aws/ubuntu/redis:latest",
     "essential": true,
-    "name": "redis",
-    "dependsOn": [ 
-      { 
-        "containerName": "envoy",
-        "condition": "HEALTHY" 
-      }
-    ]
-  },
-  {
-    "logConfiguration": {
-      "logDriver": "awslogs",
-      "secretOptions": null,
-      "options": {
-        "awslogs-group": "/ecs/Ubersystem",
-        "awslogs-region": "us-east-1",
-        "awslogs-stream-prefix": "ecs"
-      }
-    },
-    "environment": [
-      {
-        "name": "APPMESH_VIRTUAL_NODE_NAME",
-        "value": "${aws_appmesh_virtual_node.redis.arn}"
-      }
-    ],
-    "image": "public.ecr.aws/appmesh/aws-appmesh-envoy:v1.23.1.0-prod",
-    "healthCheck": {
-      "retries": 3,
-      "command": [
-        "CMD-SHELL",
-        "curl -s http://localhost:9901/server_info | grep state | grep -q LIVE"
-      ],
-      "timeout": 2,
-      "interval": 5,
-      "startPeriod": 10
-    },
-    "essential": true,
-    "user": "1337",
-    "name": "envoy"
+    "name": "redis"
   }
 ]
 TASK_DEFINITION
@@ -692,80 +449,24 @@ TASK_DEFINITION
   }
 
   task_role_arn = aws_iam_role.task_role.arn
-  proxy_configuration {
-    type           = "APPMESH"
-    container_name = "envoy"
-    properties = {
-      AppPorts         = "6379"
-      EgressIgnoredIPs = "169.254.170.2,169.254.169.254"
-      IgnoredUID       = "1337"
-      ProxyEgressPort  = 15001
-      ProxyIngressPort = 15000
-    }
-  }
 }
 
-resource "aws_appmesh_virtual_service" "redis" {
-  name      = "redis.${aws_appmesh_mesh.ubersystem.name}"
-  mesh_name = aws_appmesh_mesh.ubersystem.id
-
-  spec {
-    provider {
-      virtual_node {
-        virtual_node_name = aws_appmesh_virtual_node.redis.name
-      }
-    }
-  }
+resource "aws_lb_target_group" "redis" {
+  name_prefix = "redis"
+  port        = 6379
+  protocol    = "TCP"
+  target_type = "ip"
+  vpc_id      = var.vpc_id
 }
 
-resource "aws_appmesh_virtual_node" "redis" {
-  name      = "redis"
-  mesh_name = aws_appmesh_mesh.ubersystem.id
+resource "aws_lb_listener" "redis" {
+  load_balancer_arn = aws_lb.ubersystem_internal.arn
+  port              = "6379"
+  protocol          = "TLS"
 
-  spec {
-    listener {
-      port_mapping {
-        port     = 6379
-        protocol = "tcp"
-      }
-    }
-
-    service_discovery {
-      dns {
-        hostname = "redis" #Container name
-      }
-    }
-  }
-}
-
-resource "aws_appmesh_virtual_router" "redis" {
-  name      = "redis"
-  mesh_name = aws_appmesh_mesh.ubersystem.id
-
-  spec {
-    listener {
-      port_mapping {
-        port     = 6379
-        protocol = "tcp"
-      }
-    }
-  }
-}
-
-resource "aws_appmesh_route" "redis" {
-  name                = "redis"
-  mesh_name           = aws_appmesh_mesh.ubersystem.id
-  virtual_router_name = aws_appmesh_virtual_router.redis.name
-
-  spec {
-    tcp_route {
-      action {
-        weighted_target {
-          virtual_node = aws_appmesh_virtual_node.redis.name
-          weight       = 100
-        }
-      }
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.redis.arn
   }
 }
 
@@ -782,24 +483,12 @@ resource "aws_efs_file_system" "ubersystem_static" {
 }
 
 # -------------------------------------------------------------------
-# MAGFest Envoy Service Mesh (AWS App Mesh)
+# MAGFest Task Role
 # -------------------------------------------------------------------
-
-resource "aws_appmesh_mesh" "ubersystem" {
-  name = "ubersystem"
-
-  spec {
-    egress_filter {
-      type = "ALLOW_ALL"
-    }
-  }
-}
 
 resource "aws_iam_role" "task_role" {
   name_prefix = "uber"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -815,8 +504,8 @@ resource "aws_iam_role" "task_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "task_role_envoy" {
-  role       = aws_iam_role.task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSAppMeshEnvoyAccess"
-}
+# resource "aws_iam_role_policy_attachment" "task_role_envoy" {
+#   role       = aws_iam_role.task_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AWSAppMeshEnvoyAccess"
+# }
 
