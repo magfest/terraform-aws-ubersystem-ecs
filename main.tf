e21b947f1c0fcb82fd4bd778a129fcaef612f24d
Jason Spriggs <jason.spriggs@magfest.org>
2022-11-05 20:27:25 -0400

    Switch to HTTP

--- main.tf
+++ main.tf
@@ -52,10 +52,10 @@ resource "aws_lb_target_group" "ubersystem_web" {
 
 resource "aws_lb_listener" "ubersystem_web" {
   load_balancer_arn = aws_lb.ubersystem.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.cert_arn
+  port              = "80"
+  protocol          = "HTTP"
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #certificate_arn   = var.cert_arn
 
   default_action {
     type             = "forward"
