e3bd1dd78d3c3cf9a557be4877360b7c4ef6dc78
Jason Spriggs <jason.spriggs@magfest.org>
2022-11-05 20:38:12 -0400

    Add HTTPS back

--- main.tf
+++ main.tf
@@ -63,6 +63,19 @@ resource "aws_lb_listener" "ubersystem_web_http" {
   }
 }
 
+resource "aws_lb_listener" "ubersystem_web_https" {
+  load_balancer_arn = aws_lb.ubersystem.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.cert_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ubersystem_web.arn
+  }
+}
+
 resource "aws_lb" "ubersystem_internal" {
   name_prefix        = "uber"
   internal           = true
